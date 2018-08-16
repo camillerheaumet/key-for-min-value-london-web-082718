bda3639ff5ef5059d133c96c9dd0fb65cdd8971d
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-16 10:18:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
 
   else name_hash.each_with_index do |value, index|
-    if name_hash[ i ] > name_hash[ i + 1 ]
+    next_value = name_hash.to_a[i+1].nil? ? 0 : name_hash.to_a[i+1]
+    name_hash.delete_if name_hash[i] > next_value
       
 
   end
